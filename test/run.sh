e84ae6209bdf63f7493ca4dc3931ed5992812778
Chris Weyl <cweyl@alumni.drew.edu>
2018-03-28 14:31:28 -0500

    Fix our travis invocation
    This should get everything cloned and in place properly, at least.

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-[ -d vader.vim     ] || git clone --depth=1 https://github.com/junegunn/vader.vim.git
-[ -d vimi-ducttape ] || git clone --depth=1 https://github.com/rsrchboy/vim-ducttape.git
+_pp='pack/travis/start'
+_clone="-C $_pp clone --depth=1 --recurse-submodule"
 
-# _LIB=$(find p5/ -maxdepth 2 -name lib -type d -printf ':%p')
-# find autoload -name '*.pm' | PERL5LIB=$PERL5LIB:$_LIB xargs -L1 perl -I autoload -I lib -I t/lib -MVIM -wc 2>&1
+mkdir -p $_pp
+[ -d $_pp/vader.vim    ] || git $_clone --depth=1 https://github.com/junegunn/vader.vim.git
+[ -d $_pp/vim-ducttape ] || git $_clone --depth=1 https://github.com/rsrchboy/vim-ducttape.git
 
 # vim *MUST* also load a file here for all tests to pass
-vim -Nu test/vimrc -c 'Vader! test/*' LICENSE 2>&1
+vim --clean -Nnu test/vimrc -c 'Vader! test/*' LICENSE
